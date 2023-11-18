ca798d7502d89bead7224f2d5d77f6b42d0f947a
James Rees <jarees@gmail.com>
2023-11-18 16:41:41 +0100

    Add Sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  mount Sidekiq::Web, at: 'sidekiq'
   root 'home#index'
 
   resources :councils, only: %i[index show]
