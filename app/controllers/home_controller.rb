498c49a37e9f3e9c83613b39b6ced0d742f3ad05
Rich <52744614+richlovescats@users.noreply.github.com>
2023-11-18 14:38:24 +0000

    add filter on home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,24 +1,37 @@
 class HomeController < ApplicationController
   def index
-    meetings = assemble_meetings
-    decisions = assemble_decisions
-
-    # Tag each item with its type and combine the arrays
-    tagged_meetings = meetings.map { |item| { kind: 'meeting', item: item } }
-    tagged_decisions = decisions.map { |item| { kind: 'decision', item: item } }
-    combined_items = tagged_meetings + tagged_decisions
-
-    # Sort the combined array by the created_at attribute of each item
-    @feed_items = combined_items.sort_by { |hash| hash[:item].created_at }.reverse
+    @council = council_from_params || nil
+    @feed_items = assemble_feed_items(@council)
   end
 
   private
 
-  def assemble_meetings
-    Meeting.order(id: :desc).take(10)
+  def council_from_params
+    Council.find_by(id: params[:council_id]) if params[:council_id].present?
   end
 
-  def assemble_decisions
-    Decision.order(id: :desc).take(10)
+  def assemble_feed_items(council)
+    items = [assemble_meetings(council), assemble_decisions(council)].flatten
+    tag_and_sort_items(items)
+  end
+
+  def assemble_meetings(council)
+    scope = council ? council.meetings : Meeting
+    scope.order(id: :desc).limit(10)
+  end
+
+  def assemble_decisions(council)
+    scope = council ? council.decisions : Decision
+    scope.order(id: :desc).limit(10)
+  end
+
+  def tag_and_sort_items(items)
+    items.map { |item| tag_item(item) }
+         .sort_by { |hash| -hash[:item].created_at.to_i }
+  end
+
+  def tag_item(item)
+    kind = item.is_a?(Meeting) ? 'meeting' : 'decision'
+    { kind: kind, item: item }
   end
 end
--- app/models/council.rb
+++ app/models/council.rb
@@ -1,5 +1,6 @@
 class Council < ApplicationRecord
   has_many :meetings
+  has_many :decisions
   has_many :committees
   has_many :people, class_name: 'Person'
 end
