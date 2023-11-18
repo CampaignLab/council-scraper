8bfe7a1ab1c5121eb5a1d368200b8064f2a9f870
Rich <52744614+richlovescats@users.noreply.github.com>
2023-11-18 15:34:24 +0000

    add tag filtering

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,22 +1,28 @@
 class HomeController < ApplicationController
   def index
     @council = council_from_params || nil
-    @feed_items = assemble_feed_items(@council)
+    @tag = tag_from_params || nil
+    @feed_items = assemble_feed_items(@council, @tag)
   end
 
   private
+
+  def tag_from_params
+    Tag.find_by(id: params[:tag_id]) if params[:tag_id].present?
+  end
 
   def council_from_params
     Council.find_by(id: params[:council_id]) if params[:council_id].present?
   end
 
-  def assemble_feed_items(council)
-    items = [assemble_meetings(council), assemble_decisions(council)].flatten
+  def assemble_feed_items(council, tag)
+    items = [assemble_meetings(council, tag), assemble_decisions(council)].flatten
     tag_and_sort_items(items)
   end
 
-  def assemble_meetings(council)
+  def assemble_meetings(council, tag)
     scope = council ? council.meetings.with_minutes : Meeting.with_minutes
+    scope = scope.joins(:meeting_tags).where(meeting_tags: { tag_id: tag.id }) if tag
     scope.where.not(date: nil).order(date: :desc).limit(10)
   end
 
