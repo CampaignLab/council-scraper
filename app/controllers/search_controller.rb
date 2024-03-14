adf4ee5d531c2331f0b19393647b9334f9cceb50
James Rees <jarees@gmail.com>
2024-03-14 00:06:57 +0000

    Improve search layout and highlighting, add option to search openAI summaries

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,19 +1,21 @@
 class SearchController < ApplicationController
   def index
-    if params[:query].present?
+    @query = params[:query]
+    @council_id = params[:council_id]
+    @mode = params[:mode] || 'document'
+
+    if @query.present?
       filters = {}
-      if params[:council_id].present?
-        filters[:organisation_ids] = [params[:council_id]]
+      if @council_id.present?
+        filters[:organisation_ids] = [@council_id]
       end
-      @results = Integrations::Opensearch.new.search(
-        params[:query],
+      @results = Integrations::Opensearch.new(mode: @mode).search(
+        @query,
         filters
       )['hits']['hits'].map do |hit|
         hit['object'] = Document.find(hit['_source']['id'])
         hit
       end
-      @query = params[:query]
-      @council_id = params[:council_id]
     end
   end
 
